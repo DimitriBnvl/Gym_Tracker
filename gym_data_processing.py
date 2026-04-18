6634629dee1edf3fdb4f1aa078ca02957a5b85d9
Dimitri Bonneville <dimitribonneville1@gmail.com>
2026-04-18 13:21:55 +0100

    Add post_in_sheet and make it work for one exercise for now

--- gym_data_processing.py
+++ gym_data_processing.py
@@ -1,24 +1,28 @@
 import os
 import requests
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
 class GymDataProcessing:
     def __init__(self, query, weight, height, age, gender):
-        self.exercise_url    = "https://app.100daysofpython.dev"
+        self.exercise_url      = "https://app.100daysofpython.dev"
         self.exercise_post_url = f"{self.exercise_url}/v1/nutrition/natural/exercise"
+        self.sheety_post_url   = f"https://api.sheety.co/12b1cd54a019188aeb95639019259c73/workoutTracker/workouts"
+
         self.exercise_params = {
-            "query": query,
+            "query"    : query,
             "weight_kg": weight,
-            "heigh_cm": height,
-            "age": age,
-            "gender": gender,
+            "heigh_cm" : height,
+            "age"      : age,
+            "gender"   : gender,
         }
+
         self.headers = {
             "Content-Type": "application/json",
-            "x-app-id": os.environ.get("EXERCISE_API_APP_ID"),
-            "x-app-key": os.environ.get("EXERCISE_API_KEY"),
+            "x-app-id"    : os.environ.get("EXERCISE_API_APP_ID"),
+            "x-app-key"   : os.environ.get("EXERCISE_API_KEY"),
         }
 
         self.query_guidelines = """
@@ -33,6 +37,31 @@ class GymDataProcessing:
         """
 
     def post_exercise(self):
-        response = requests.post(url=self.exercise_post_url, json=self.exercise_params, headers=self.headers)
-        response.raise_for_status()
-        result = response.json()
+        exercise_data = requests.post(url=self.exercise_post_url, json=self.exercise_params, headers=self.headers)
+        exercise_data.raise_for_status()
+        exercises = exercise_data.json()
+        return exercises["exercises"]
+
+    def post_in_sheet(self):
+        exercises_list = self.post_exercise()
+
+        date_now = datetime.today().strftime('%d/%m/%Y')
+        time_now = datetime.today().strftime("%H:%M:%S")
+
+        for exercise in exercises_list:
+            exercise_name = exercise["name"]
+            exercise_duration = exercise["duration_min"]
+            exercise_calories = exercise["nf_calories"]
+
+            sheets_params = {
+                "workout": {
+                    "date": date_now,
+                    "time": time_now,
+                    "exercise": exercise_name.title(),
+                    "duration": exercise_duration,
+                    "calories": exercise_calories,
+                }
+            }
+
+            sheet_data = requests.post(url=self.sheety_post_url, json=sheets_params)
+            sheet_data.raise_for_status()
--- main.py
+++ main.py
@@ -5,10 +5,11 @@ HEIGHT_CM = 182
 AGE = 19
 GENDER = "male"
 
-query = input("Enter query: ")
+# query = input("Enter query: ")
+query = "I ran 8 kilometers and swam for 20 min"
 
 if __name__ == '__main__':
     data_processor = GymDataProcessing(query, WEIGHT_KG, HEIGHT_CM, AGE, GENDER)
-    data_processor.post_exercise()
+    print(data_processor.post_in_sheet())
 
 
